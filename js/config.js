79a47b5a0f64a278aa2ba213058fe6eb1b2d3e03
Vivek455851 <vivektu45@gmail.com>
2025-03-09 11:07:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_8PX09DoBb',
         userPoolClientId: '518fs7n6ms928vnm56p15nstq4',
-        region: 'ap-south'
+        region: 'ap-south-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
